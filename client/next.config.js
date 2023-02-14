cbedff127ac37e23d115bdec56d20e215689c4ec
aleksandr-kramer <kramer4507@gmail.com>
2023-02-14 11:54:34 +0100

    create git ignore and next.config,js

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,9 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  i18n: {
+    locales: ["en", "es", "ru"],
+    defaultLocale: "en",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
