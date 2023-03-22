ec7458970553208a7d30f98b72e702faf9c8920b
aleksandr-kramer <kramer4507@gmail.com>
2023-03-22 11:53:29 +0100

    create model navigation

--- controllers/index-controller.js
+++ controllers/index-controller.js
@@ -1,6 +1,11 @@
 require("../models/LandingMonoLink");
 const Logo = require("../models/Logo");
 const Socialnetwork = require("../models/Socialnetwork");
+const {
+  Navigation_en,
+  Navigation_es,
+  Navigation_ru,
+} = require("../models/Navigation");
 const { Page_en, Page_es, Page_ru } = require("../models/Page");
 const {
   AboutStSp_en,
@@ -12,13 +17,22 @@ const getPageIndexEn = async (req, res) => {
   try {
     const logodata = await Logo.findOne({ _id: "641859e21127a88a8a58f32d" });
     const socialnetworkdata = await Socialnetwork.find({ locale: "en" });
+    const navigationdata = await Navigation_en.findOne({
+      _id: "641a283568c667addfdb882f",
+    });
     const indexpagedata = await Page_en.findOne({ name: "index" }).populate(
       "LandingMonoLink"
     );
     const aboutstspdata = await AboutStSp_en.findOne({
       _id: "63efa08c2dc00ae4493c2872",
     });
-    res.json({ logodata, socialnetworkdata, indexpagedata, aboutstspdata });
+    res.json({
+      logodata,
+      socialnetworkdata,
+      navigationdata,
+      indexpagedata,
+      aboutstspdata,
+    });
   } catch (error) {
     console.log(error);
   }
@@ -28,13 +42,20 @@ const getPageIndexEs = async (req, res) => {
   try {
     const logodata = await Logo.findOne({ _id: "641859e21127a88a8a58f32d" });
     const socialnetworkdata = await Socialnetwork.find({ locale: "en" });
+    const navigationdata = await Navigation_es.find();
     const indexpagedata = await Page_es.findOne({ name: "index" }).populate(
       "LandingMonoLink"
     );
     const aboutstspdata = await AboutStSp_es.findOne({
       _id: "63efa08c2dc00ae4493c2872",
     });
-    res.json({ logodata, socialnetworkdata, indexpagedata, aboutstspdata });
+    res.json({
+      logodata,
+      socialnetworkdata,
+      navigationdata,
+      indexpagedata,
+      aboutstspdata,
+    });
   } catch (error) {
     console.log(error);
   }
@@ -44,13 +65,20 @@ const getPageIndexRu = async (req, res) => {
   try {
     const logodata = await Logo.findOne({ _id: "641859e21127a88a8a58f32d" });
     const socialnetworkdata = await Socialnetwork.find({ locale: "ru" });
+    const navigationdata = await Navigation_ru.find();
     const indexpagedata = await Page_ru.findOne({ name: "index" }).populate(
       "LandingMonoLink"
     );
     const aboutstspdata = await AboutStSp_ru.findOne({
       _id: "63efa08c2dc00ae4493c2872",
     });
-    res.json({ logodata, socialnetworkdata, indexpagedata, aboutstspdata });
+    res.json({
+      logodata,
+      socialnetworkdata,
+      navigationdata,
+      indexpagedata,
+      aboutstspdata,
+    });
   } catch (error) {
     console.log(error);
   }
---
+++ models/Navigation.js
@@ -0,0 +1,64 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const navigationSchema = new Schema({
+  subdomainlink: [
+    {
+      id: Number,
+      subdomainname: String,
+      subdomainurl: String,
+    },
+  ],
+  domainlink: [
+    {
+      id: Number,
+      domainname: String,
+      domainurl: String,
+    },
+  ],
+  navmenu: [
+    {
+      id: Number,
+      namemenu: String,
+      submenu: [
+        {
+          id: Number,
+          titlesubmenu: String,
+          listsubmenu: [
+            {
+              id: Number,
+              namesubmenu: String,
+              urlsubmenu: String,
+            },
+          ],
+        },
+      ],
+      landingmenu: [
+        {
+          id: Number,
+          titlelandingmenu: String,
+          textlandingmenu: String,
+          urllandingmenu: String,
+        },
+      ],
+    },
+  ],
+});
+
+const Navigation_en = mongoose.model(
+  "Navigation_en",
+  navigationSchema,
+  "navigation_en"
+);
+const Navigation_es = mongoose.model(
+  "Navigation_es",
+  navigationSchema,
+  "navigation_es"
+);
+const Navigation_ru = mongoose.model(
+  "Navigation_ru",
+  navigationSchema,
+  "navigation_ru"
+);
+
+module.exports = { Navigation_en, Navigation_es, Navigation_ru };
